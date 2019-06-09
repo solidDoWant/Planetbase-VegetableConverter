df2edf298bc8d47dd459f5f1bd012f02385de4e9
Fred Heinecke <fheinecke@ksu.edu>
2019-06-09 10:31:38 -0500

    Initial upload

---
+++ Planetbase-VegetableConverter/VegetableConverter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Planetbase;
+using UnityEngine;
+
+namespace VegetableConverter
+{
+    //TODO At some point I'll move a lot of this boilerplate code to the framework
+    public class VegetableConverter : ComponentType
+    {
+        //This component will pull in some information (icon, 3d model) from the bioplastic processor
+        private static ComponentType ComponentInstance { get; } =
+            TypeList<ComponentType, ComponentTypeList>.find<BioplasticProcessor>();
+
+        public VegetableConverter()
+        {
+            //The cost to build the component
+            mConstructionCosts = new ResourceAmounts();
+            mConstructionCosts.add(TypeList<ResourceType, ResourceTypeList>.find<Metal>(), 1);
+
+            //The icon to display when selecting the component on the module's component build menu.
+            //This can also be loaded from the assets folder via mod.ModTextures.FindTextureWithName("icon.png");
+            mIcon = ComponentInstance.getIcon();
+
+            //The resources the component uses for each item it produces
+            mResourceConsumption = new List<ResourceType> { TypeList<ResourceType, ResourceTypeList>.find<Vegetables>() };
+
+            //The resource to produce
+            addResourceProduction<Starch>();
+
+            //The amount of the raw resources the component can hold at a time
+            mEmbeddedResourceCount = 3;
+
+            //How long (in seconds I think) it takes to produce one item
+            mResourceProductionPeriod = 100f;
+
+            //How much power the machine continuously uses (or produces)
+            mPowerGeneration = -1000;
+
+            //Various settings are configured by adding or bitwise-or flag constants together
+            mFlags = FlagRequiresOperator + FlagCanBeDisabled + FlagStoreProduction;
+
+            //Set who can use the component
+            mOperatorSpecialization = TypeList<Specialization, SpecializationList>.find<Worker>();
+
+            //Set the animation for working on the component
+            addUsageAnimation(CharacterAnimationType.WorkStanding);
+
+            mName = "Vegetable Converter";
+            mTooltip = "Turns vegetables into starch.";
+        }
+
+        public override GameObject createModel() => ComponentInstance.createModel();
+    }
+}
---
+++ Planetbase-VegetableConverter/VegetableConverterMod.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Planetbase;
+using PlanetbaseFramework;
+
+namespace VegetableConverter
+{
+    public class VegetableConverterMod : ModBase
+    {
+        public override string ModName { get; } = "VegetableConverter";
+
+        public override void Init()
+        {
+            var converterInstance = (ComponentType)new VegetableConverter();
+
+            //Register the component
+            var componentTypeList = TypeList<ComponentType, ComponentTypeList>.getInstance();
+            componentTypeList.add(converterInstance);
+
+            //Add the component to the biodome. Unfortunately this also must be done with reflection
+            var bioDome = TypeList<ModuleType, ModuleTypeList>.find<ModuleTypeBioDome>();
+
+            var newComponentList = bioDome.mComponentTypes.ToList();
+            newComponentList.Add(converterInstance);
+            bioDome.mComponentTypes = newComponentList.ToArray();
+        }
+    }
+}
